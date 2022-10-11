d53937017f606c3d2556270423ee5198883fb29a
LuisHartmannC <lhartmann@uninorte.edu.co>
2022-10-10 19:02:52 -0500

    Added SignUp

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,43 +1,130 @@
-import React from 'react'
+import "bootstrap/dist/css/bootstrap.min.css"
+import "./Home.css"
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
+import React, { useState } from "react"
+
 
 export const Home = () => {
-  return (
+    let [authMode, setAuthMode] = useState("signin")
   
-<div className="container">
-			<h1>INSTA YA - APP </h1>
-			
-			<div className="row">
-                <div id="login" className="col-lg-4 offset-lg-4 col-md-6 offset-md-3
-                    col-12">
-                    <h2 className="text-center">Bienvenido de nuevo</h2>
-                    
-                    <form>
-                        <div className="form-group">
-                            <label for="correo">Correo</label>
-                            <input id="correo" name="correo"
-                                className="form-control" type="email"
-                                placeholder="Correo electrónico" />
-                        </div>
-                        <div className="form-group">
-                            <label for="palabraSecreta">Contraseña</label>
-                            <input id="palabraSecreta" name="palabraSecreta"
-                                className="form-control" type="password"
-                                placeholder="Contraseña" />
-                        </div>
-                        <button type="submit" class="btn btn-primary mb-2">
-                            Entrar
-                        </button>
-                        
-                        
-                    </form>
-                </div>
+    const changeAuthMode = () => {
+      setAuthMode(authMode === "signin" ? "signup" : "signin")
+    }
+  
+    if (authMode === "signin") {
+      return (
+        <div className="Auth-form-container">
+          <form className="Auth-form">
+            <div className="Auth-form-content">
+              <h3 className="Auth-form-title">Sign In</h3>
+              <div className="text-center">
+                ¿No tiene una cuenta?{" "}
+                <span className="link-primary" onClick={changeAuthMode}>
+                  Registrarse
+                </span>
+              </div>
+              <div className="form-group mt-3">
+                <label>Correo Electronico</label>
+                <input
+                  type="email"
+                  className="form-control mt-1"
+                  placeholder="EX: 12345@asdf.com"
+                />
+              </div>
+              <div className="form-group mt-3">
+                <label>Contraseña</label>
+                <input
+                  type="password"
+                  className="form-control mt-1"
+                  placeholder="Contraseña"
+                />
+              </div>
+              <div className="d-grid gap-2 mt-3">
+                <Link to="/">
+                  <button type="submit" className="btn btn-primary">
+                    Ingresar
+                  </button>
+                </Link>
+              </div>
+              <p className="text-center mt-2">
+                <a href="#">¿Olvido la Contraseña?</a>
+              </p>
             </div>
-			
-				
-			
-		</div>
-
-
+          </form>
+        </div>
+      )
+    }
   
-  )
-}
+    return (
+      <div className="Auth-form-container">
+        <form className="Auth-form">
+          <div className="Auth-form-content">
+            <h3 className="Auth-form-title">Crear Cuenta</h3>
+            <div className="text-center">
+              ¿Ya posee una cuenta?{" "}
+              <span className="link-primary" onClick={changeAuthMode}>
+                Ingresar
+              </span>
+            </div>
+            <div className="form-group mt-3">
+              <label>Nombres</label>
+              <input
+                type="email"
+                className="form-control mt-1"
+                placeholder="e.g Ana Maria"
+              />
+            </div>
+            <div className="form-group mt-3">
+              <label>Apellidos</label>
+              <input
+                type="email"
+                className="form-control mt-1"
+                placeholder="e.g Perez Sanchez"
+              />
+            </div>
+            <div className="form-group mt-3">
+              <label>Correo Electronico</label>
+              <input
+                type="email"
+                className="form-control mt-1"
+                placeholder="EX: 12345@asdf.com"
+              />
+            </div>
+            <div className="form-group mt-3">
+              <label>Contraseña</label>
+              <input
+                type="password"
+                className="form-control mt-1"
+                placeholder="Contraseña"
+              />
+            </div>
+            <div className="form-group mt-3">
+              <label>Repita su Contraseña</label>
+              <input
+                type="password"
+                className="form-control mt-1"
+                placeholder="Contraseña"
+              />
+            </div>
+            <div class="form-check d-flex align-items-start mb-3">
+              <input class="form-check-input me-5" type="checkbox" value="" id="checkBoxId" name="check" oninput="this.className = ''" />
+              <div>
+                <label class="form-check-label" for="form2Example3">
+                  Estoy de acuerdo con los <a href="{{ url_for('static', filename='Privacidad.pdf') }}">Terminos de Servicio</a>
+                </label></div>
+            </div>
+            <div className="d-grid gap-2 mt-3">
+              <button type="submit" className="btn btn-primary">
+                Crear Cuenta
+              </button>
+            </div>
+          </div>
+        </form>
+      </div>
+    )
+  }
